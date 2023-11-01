d90770622e682dd8cf8f6814b07dfbe86fcb8d84
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-01 14:44:44 -0700

    complete lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,28 @@
 class MySet:
+    """Here is my doc string..."""
 
-    pass
+    def __init__(self, enumerable=[]):
+        self.dictionary = {}
+        for val in enumerable:
+            self.dictionary[val] = True
+
+    def __str__(self):
+        return "MySet: {" + ",".join([str(k) for k in self.dictionary]) + "}"
+
+    def has(self, val):
+        return val in self.dictionary
+
+    def add(self, val):
+        self.dictionary[val] = True
+        return self
+
+    def delete(self, val):
+        self.dictionary.pop(val, None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        self.dictionary = {}
+        return self
